198176945aa2dc98856df47016fc1d8ebbd2bdcc
Neis_k <kolesnik.an@list.ru>
2026-06-09 12:43:11 +0300

    Add read, write and mkdir commands in FileSystemB

--- src/lib/filesystem_b.cpp
+++ src/lib/filesystem_b.cpp
@@ -64,4 +64,94 @@ IFileSystem::path_type FileSystemB::join_path(const path_type& parent, const pat
     return parent + "/" + name;
 }
 
+IFileSystem::bytes_type FileSystemB::op_read(const path_type& path) const {
+    const Node* node = get_node(path);
+
+    if(!node->is_file) {
+        throw std::runtime_error("read from directory");
+    }
+
+    return node->data;
+}
+
+void FileSystemB::op_mkdir(const path_type& path) {
+    if(path == "/") {
+        return;
+    }
+
+    if(index_.find(path) != index_.end()) {
+        throw std::runtime_error("path already exists");
+    }
+
+    path_type parent = parent_path(path);
+    path_type name = basename(path);
+
+    Node* parent_node = get_node(parent);
+
+    if(parent_node->is_file) {
+        throw std::runtime_error("parent is file");
+    }
+
+    auto new_node = std::make_unique<Node>();
+    new_node->name = name;
+    new_node->is_file = false;
+    new_node->parent = parent_node;
+
+    Node* raw = new_node.get();
+
+    parent_node->children.push_back(std::move(new_node));
+    index_[path] = raw;
+}
+
+void FileSystemB::op_write(const path_type& path, const bytes_type& data) {
+    auto it = index_.find(path);
+
+    if(it != index_.end()) {
+        Node* node = it->second;
+
+        if(!node->is_file) {
+            throw std::runtime_error("write to directory");
+        }
+
+        node->data = data;
+        return;
+    }
+
+    path_type parent = parent_path(path);
+    path_type name = basename(path);
+
+    Node* parent_node = get_node(parent);
+
+    if(parent_node->is_file) {
+        throw std::runtime_error("parent is file");
+    }
+
+    auto new_node = std::make_unique<Node>();
+    new_node->name = name;
+    new_node->is_file = true;
+    new_node->data = data;
+    new_node->parent = parent_node;
+
+    Node* raw = new_node.get();
+
+    parent_node->children.push_back(std::move(new_node));
+    index_[path] = raw;
+}
+
+IFileSystem::units_list_type FileSystemB::op_ls(const path_type&) const {
+    throw std::runtime_error("op_ls is not implemented");
+}
+
+void FileSystemB::op_mv(const path_type&, const path_type&) {
+    throw std::runtime_error("op_mv is not implemented");
+}
+
+IFileSystem::units_list_type FileSystemB::op_find(const path_type&, const path_type&) const {
+    throw std::runtime_error("op_find is not implemented");
+}
+
+size_t FileSystemB::get_memory_usage() const noexcept {
+    return 0;
+}
+
 } // namespace filesystem
