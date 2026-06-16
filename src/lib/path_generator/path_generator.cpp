e933107356caeacf30dc34f49fc9c0469b65d550
Igor <kondratyevigor200730@gmail.com>
2026-06-16 17:02:02 +0300

    zipf distrib implemented & path generator done

--- src/lib/path_generator/path_generator.cpp
+++ src/lib/path_generator/path_generator.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
 #include <random>
 #include <string>
 #include <vector>
@@ -55,7 +58,7 @@ namespace pathgen {
                     break;
 
                 case OperationType::Find:
-                    result.push_back(generated_fs_.get_path(gen_dir_()) + "/*");
+                    result.push_back(gen_pattern_());
                     break;
             }
         }
@@ -67,9 +70,8 @@ namespace pathgen {
     UniformPathGenerator::UniformPathGenerator(const fsgenerator::GeneratedFs& fs, double loc)
         : IPathGenerator(fs, loc) {}
 
-
     size_t UniformPathGenerator::gen_file_() noexcept {
-        size_t cur = -1;
+        size_t cur;
         if (last_dir_ != -1 && !children_[last_dir_].files.empty() && rnd_.probability(loc_)) {
             cur = rnd_.choice(children_[last_dir_].files);
         } else {
@@ -81,7 +83,7 @@ namespace pathgen {
         return cur;
     }
     size_t UniformPathGenerator::gen_dir_() noexcept {
-        size_t cur = -1;
+        size_t cur;
         if (last_dir_ != -1 && !children_[last_dir_].dirs.empty() && rnd_.probability(loc_)) {
             cur = rnd_.choice(children_[last_dir_].dirs);
         } else {
@@ -91,6 +93,63 @@ namespace pathgen {
 
         last_dir_ = generated_fs_.get_node(cur).parent;
         return cur;
+    }
+    IPathGenerator::path_type UniformPathGenerator::gen_pattern_() noexcept {
+        return generated_fs_.get_path(gen_dir_()) + "/*";
+    }
+
+    ZipfPathgenerator::ZipfPathgenerator(const fsgenerator::GeneratedFs& fs, double s, double loc)
+        : IPathGenerator(fs, loc)
+        , s_(s)
+        , files_distr_(0, fs.get_files().size() - 1, s_)
+        , dirs_distr_(0, fs.get_dirs().size() - 1, s_)
+        , rank_to_file_(fs.get_files())
+        , rank_to_dir_(fs.get_dirs())
+    {
+        std::mt19937_64 g;
+        std::shuffle(rank_to_file_.begin(), rank_to_file_.end(), g);
+        std::shuffle(rank_to_dir_.begin(), rank_to_dir_.end(), g);
+
+        size_t W = 0;
+        for (size_t i : generated_fs_.get_dirs()) {
+            W = std::max(W, generated_fs_.get_node(i).children);
+        }
+
+        small_distrs_.reserve(W + 1);
+        small_distrs_.emplace_back(0, 0, s_);
+        for (size_t n = 1; n < W + 1; ++n) {
+            small_distrs_.emplace_back(0, n-1, s_);
+        }
+    }
+
+    size_t ZipfPathgenerator::gen_file_() noexcept {
+        size_t cur;
+        if (last_dir_ != -1 && !children_[last_dir_].files.empty() && rnd_.probability(loc_)) {
+            cur = children_[last_dir_].files[
+                rnd_.generate(small_distrs_[children_[last_dir_].files.size()])
+            ];
+        } else {
+            cur = rank_to_file_[rnd_.generate(files_distr_)];
+        }
+
+        last_dir_ = generated_fs_.get_node(cur).parent;
+        return cur;
+    }
+    size_t ZipfPathgenerator::gen_dir_() noexcept {
+        size_t cur;
+        if (last_dir_ != -1 && !children_[last_dir_].dirs.empty() && rnd_.probability(loc_)) {
+            cur = children_[last_dir_].dirs[
+                rnd_.generate(small_distrs_[children_[last_dir_].dirs.size()])
+            ];
+        } else {
+            cur = rank_to_dir_[rnd_.generate(dirs_distr_)];
+        }
+
+        last_dir_ = generated_fs_.get_node(cur).parent;
+        return cur;
+    }
+    IPathGenerator::path_type ZipfPathgenerator::gen_pattern_() noexcept {
+        return generated_fs_.get_path(gen_dir_()) + "/*";
     }
 
 } // namespace pathgen
--- src/lib/path_generator/path_generator.hpp
+++ src/lib/path_generator/path_generator.hpp
@@ -7,7 +7,7 @@
 #include "../filesystem.hpp"
 #include "../fs_generator/generated_fs.hpp"
 #include "../random/random.hpp"
-#include "external/zipfian_int_distribution.h"
+#include "zipf_distribution.hpp"
 
 namespace pathgen {
     
@@ -25,6 +25,7 @@ namespace pathgen {
 
         virtual size_t gen_file_() noexcept = 0;
         virtual size_t gen_dir_() noexcept = 0;
+        virtual path_type gen_pattern_() noexcept = 0;
 
         const fsgenerator::GeneratedFs& generated_fs_;
         double loc_;
@@ -44,11 +45,11 @@ namespace pathgen {
     public:
 
         UniformPathGenerator(const fsgenerator::GeneratedFs& fs, double loc);
-        // std::vector<path_type> generate(const std::vector<OperationType>&) noexcept override;
         
     private:
         size_t gen_file_() noexcept override;
         size_t gen_dir_() noexcept override;
+        path_type gen_pattern_() noexcept override;
     };
 
 
@@ -56,11 +57,20 @@ namespace pathgen {
     public:
 
         ZipfPathgenerator(const fsgenerator::GeneratedFs& fs, double s, double loc);
-        // std::vector<path_type> generate(const std::vector<OperationType>&) noexcept override;
 
     private:
         size_t gen_file_() noexcept override;
         size_t gen_dir_() noexcept override;
+        path_type gen_pattern_() noexcept override;
+
+        double s_;
+        ZipfDistribution<size_t> files_distr_;
+        ZipfDistribution<size_t> dirs_distr_;
+
+        std::vector<size_t> rank_to_file_;
+        std::vector<size_t> rank_to_dir_;
+
+        std::vector<ZipfDistribution<size_t>> small_distrs_;
     };
 
 } // namespace pathgen
